bcc52b8169f3320d50be127e399dcbe1df9e29e0
Hugo Nogueira <hnogueira@marabyte.com>
2023-07-16 11:52:51 +0100

    conditionally import prettier

--- packages/typed-css/src/formatters.ts
+++ packages/typed-css/src/formatters.ts
@@ -1,12 +1,11 @@
-import { format, resolveConfig } from "prettier";
-
 export async function formatWithPrettier(
 	code: string,
 ): Promise<string | undefined> {
 	let formatted: string | undefined;
 	try {
-		const options = await resolveConfig(process.cwd());
-		formatted = await format(code, options ?? undefined);
+		const prettier = await import("prettier");
+		const options = await prettier.resolveConfig(process.cwd());
+		formatted = await prettier.format(code, options ?? undefined);
 	} catch (error) {
 		if (error instanceof Error) {
 			throw new Error(
